82d050396d067280c36ae25328e2e1933fb6951c
shahidkhot717 <shahidkhot717@gmail.com>
2019-01-12 09:44:56 +0530

    expt1

---
+++ linear_regression.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+dataset=pd.read_csv('Salary_Data.csv')
+X=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,-1:].values# -
+
+
+#*- coding: utf-8 -*-
+from sklearn.model_selection import train_test_split
+
+
+X_train , X_test , y_train, y_test = train_test_split(X,y,test_size= 1/3, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+y_pred = regressor.predict(X_test)
+
+
+plt.scatter(X_train,y_train,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Experience(Training set)')
+plt.xlabel('years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+plt.scatter(X_test,y_test,color='red')
+plt.plot(X_test,regressor.predict(X_test),color='blue')
+plt.title('Salary vs Experience(Test set)')
+plt.xlabel('years of Experience')
+plt.ylabel('Salary')
+plt.show()
