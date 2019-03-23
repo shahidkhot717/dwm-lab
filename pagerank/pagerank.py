0f366b4991f05cffe62c43f68fc807f6d2c390d7
shahidkhot717 <shahidkhot717@gmail.com>
2019-03-23 10:02:25 +0530

    tree

---
+++ pagerank/pagerank.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 23 09:47:43 2019
+
+@author: aiktc
+"""
+
+import numpy as np
+N=int(input('no of pages'))
+d=0.85
+eps=a.4e-8
+print("enter adjacency matrix")
+print("type 1 if there is a link from a page i to page j else type 0")
+links[]
+for i in range(0,N):
+    l[]
+    for j in range(0,N):
+        L.append(int(input('page' +str(i+1)+'to page'+str(j+1)+': '+ )))
+        links.append(L)
+outboundL =np.zeros((N,),dtype=int)
+for i in range(0,N):
+    for Jin range(o,N):
+        if links[j][i] == 1:
+            M[i][j] = 1/outboundL[j]
+            
+        
+        
